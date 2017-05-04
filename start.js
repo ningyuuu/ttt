e8caea587f65bdbc8e75cf6cbaa62a4c63d4f37d
Ning Yu <ningyu321@gmail.com>
2017-05-04 23:40:04 +0800

    Initial commit

---
+++ start.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log('Hello world server!');
+});
